812fca172fdcad9d698ab778da13dc4adc5e6a76
Rahul Somasundaram <rahul.somasundaram@checkpt.com>
2021-10-12 21:01:59 +0530

    added debugging

--- build.ps1
+++ build.ps1
@@ -2,4 +2,7 @@ Invoke-WebRequest -Uri https://jrsoftware.org/download.php/is.exe -OutFile inno.
 
 Start-Process -FilePath inno.exe -ArgumentList "/VERYSILENT","/NORESTART"
 
+#for debugging
+Get-ChildItem -Path "C:/Program Files (x86)/Inno Setup 6/"
+
 Start-Process -FilePath "C:/Program Files (x86)/Inno Setup 6/ISCC.exe" -ArgumentList "helm_installer.iss"
