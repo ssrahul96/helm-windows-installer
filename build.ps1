74799da2cdb9bb5c8e9101902b34b16282877a17
Rahul Somasundaram <rahul.somasundaram@checkpt.com>
2021-10-12 20:52:33 +0530

    fixed inno installation

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri https://jrsoftware.org/download.php/is.exe -OutFile inno.exe
 
-Start-Process -FilePath inno.exe /VERYSILENT /NORESTART /ALLUSERS
+Start-Process -FilePath inno.exe -ArgumentList "/VERYSILENT","/NORESTART","/ALLUSERS"
 
 Start-Process -FilePath "C:/Program Files (x86)/Inno Setup 6/ISCC.exe" -ArgumentList "helm_installer.iss"
