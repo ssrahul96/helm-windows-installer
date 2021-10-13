1ef53d5adaef11bb0940d1abbf5676e90895f7ec
Rahul Somasundaram <ssrahul96@gmail.com>
2021-10-13 07:22:50 +0530

    added few more debugging steps

--- build.ps1
+++ build.ps1
@@ -1,8 +1,15 @@
 Invoke-WebRequest -Uri https://jrsoftware.org/download.php/is.exe -OutFile inno.exe
 
-Start-Process -FilePath inno.exe -ArgumentList "/VERYSILENT","/NORESTART"
+$process = Start-Process -FilePath inno.exe -ArgumentList "/VERYSILENT","/NORESTART" -PassThru
+
+$process.HasExited
+
+Write-Host "Process exit code : " $process.ExitCode
 
 #for debugging
 Get-ChildItem -Path ${Env:ProgramFiles(x86)}"\Inno Setup 6\"
+
+#for debugging
+Get-ChildItem -Path ${Env:ProgramFiles}"\Inno Setup 6\"
 
 Start-Process -FilePath ${Env:ProgramFiles(x86)}"\Inno Setup 6\ISCC.exe" -ArgumentList "helm_installer.iss"
